60d83a7f2138c625cd21a2206d3c5cf0a56fe4c3
gibsonac <51497641+gibsonac@users.noreply.github.com>
2019-11-11 17:44:06 -0800

    Added the Questions

--- rts_labs_code_exercise_python.py
+++ rts_labs_code_exercise_python.py
@@ -1,3 +1,6 @@
+# #1  Print the number of integers in an array that are above the given input and the number that are below, e.g. for the array [1, 5, 2, 1, 10] with input 6, print “above: 1, below: 4”.
+
+
 def Above_and_Below(givenArray, num):
     above = 0
     below = 0
@@ -12,6 +15,9 @@ def Above_and_Below(givenArray, num):
 
 
 print(Above_and_Below([1, 5, 2, 1, 10], 6))
+
+# #2  Rotate the characters in a string by a given input and have the overflow appear at the beginning, e.g. “MyString” rotated by 2 is “ngMyStri”.
+
 
 def questionTwo(givenString, num):
     if num < 0:
