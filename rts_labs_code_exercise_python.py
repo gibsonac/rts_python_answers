86b8e39781178722017782b3d7d3cc0f641da2ef
gibsonac <51497641+gibsonac@users.noreply.github.com>
2019-11-11 17:39:41 -0800

    Python Answers to questions

---
+++ rts_labs_code_exercise_python.py
@@ -0,0 +1,57 @@
+def Above_and_Below(givenArray, num):
+    above = 0
+    below = 0
+    if len(givenArray) == 0:
+        return "you need numbers in your Array!"
+    for x in range(len(givenArray)):
+        if givenArray[x] > num:
+            above += 1
+        if givenArray[x] < num:
+            below += 1
+    return (f'above: {above}, below: {below}')
+
+
+print(Above_and_Below([1, 5, 2, 1, 10], 6))
+
+def questionTwo(givenString, num):
+    if num < 0:
+        return "You need a positive number to rotate"
+    if givenString == "" or givenString == None:
+        return "you need to provide a string!"
+    num = num % len(givenString)
+    print(num)
+    tempString = ""
+    untouchedString = len(givenString) - num
+    x = len(givenString) - 1
+    while num > 0:
+        tempString = givenString[x] + tempString
+        num -= 1
+        x -= 1
+    for i in range(untouchedString):
+        tempString += givenString[i]
+    return tempString
+
+
+print(questionTwo("MyString", 2))
+
+# SECOND AND BETTER WAY
+
+
+def questionTwoSplit(givenString, num):
+    if num < 0:
+        return "You need a positive number to rotate"
+    if givenString == "" or givenString == None:
+        return "you need to provide a string!"
+    num = num % len(givenString)
+    splitLocation = len(givenString) - num
+    answer = ""
+    stringArray = [givenString[0:splitLocation],
+                   givenString[splitLocation:len(givenString)]]
+    x = len(stringArray)-1
+    while x >= 0:
+        answer = answer + stringArray[x]
+        x -= 1
+    return answer
+
+
+print(questionTwoSplit("MyString", 2))
